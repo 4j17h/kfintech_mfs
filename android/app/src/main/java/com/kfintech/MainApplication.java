5337a9903d95e30cdb57e331c8f2f4606b149e8d
4j17h <7hou74m4j17h@gmail.com>
2022-02-25 02:34:16 +0530

    Add reanimated & react-navigation/Drawer packages

--- android/app/src/main/java/com/kfintech/MainApplication.java
+++ android/app/src/main/java/com/kfintech/MainApplication.java
@@ -2,6 +2,8 @@ package com.kfintech;
 
 import android.app.Application;
 import android.content.Context;
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -32,6 +34,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+            return new ReanimatedJSIModulePackage();
         }
       };
 
